7ac6d2ba03a0caaffffd64d34d5d258987b941ff
Muhammad Shoaib Tahir <shoaibtahir410@gmail.com>
2025-07-21 21:09:50 +0500

    Fix spaCy build issue on Render

--- build.sh
+++ build.sh
@@ -2,3 +2,11 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
+#!/bin/bash
+
+echo "Installing requirements..."
+echo "Downloading NLTK corpora..."
+python download_nltk_corpora.py
+
+echo "Downloading spaCy model..."
+python -m spacy download en_core_web_sm
