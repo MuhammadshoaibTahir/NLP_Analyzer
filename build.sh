0f51e9ec7f7c2bb1428f9cba93c61dbc050c9f48
Muhammad Shoaib Tahir <shoaibtahir410@gmail.com>
2025-07-21 23:02:55 +0500

    Fix Python version and package compatibility for Render

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
-#!/usr/bin/env bash
-pip install --upgrade pip
-pip install -r requirements.txt
-python -m spacy download en_core_web_sm
 #!/bin/bash
 
-echo "Installing requirements..."
-echo "Downloading NLTK corpora..."
+pip install --upgrade pip
+pip install --prefer-binary -r requirements.txt
 python download_nltk_corpora.py
-
-echo "Downloading spaCy model..."
 python -m spacy download en_core_web_sm
